d088349497bc7a5ba79b72aaa9bcaa0af409a3c9
basiern <eferreirawagner@gmail.com>
2023-03-18 12:14:11 -0300

    Primeiro commit

---
+++ src/Operadores.java
@@ -0,0 +1,10 @@
+public class TiposVariaveis {
+    public static void main(String[] args) throws Exception {
+    
+        int numero = 5;
+        numero = numero + 1;
+
+        System.out.println(numero);
+
+    }
+}
